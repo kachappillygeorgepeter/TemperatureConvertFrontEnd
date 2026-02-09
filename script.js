3a3f86caf05a598c9e826ac54823dedeb0d719e3
kachappillygeorgepeter <kachappillygeorgepeter@gmail.com>
2026-02-09 23:06:10 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+function celsiusToFahrenheit()
+{
+    const number=document.getElementById("temperatureInput").value;
+    if(number==="")
+        document.getElementById("result").textContent="Please enter a temperature value.";
+    else
+    {
+        const result=(number*9/5)+32;
+        document.getElementById("result").textContent=`${parseFloat(number).toFixed(2)}°C = ${result.toFixed(2)}°F`;
+    }
+}
+function fahrenheitToCelsius()
+{
+    const number=document.getElementById("temperatureInput").value;
+    if(number==="")
+        document.getElementById("result").textContent="Please enter a temperature value.";
+    else
+    {
+        const result=(number-32)*5/9;
+        document.getElementById("result").textContent=`${parseFloat(number).toFixed(2)}°F = ${result.toFixed(2)}°C`;
+    }
+}
+function convert()
+{
+    const celsiusToFahrenheitRadio=document.getElementById("celsiusToFahrenheit").checked;
+    const fahrenheitToCelsiusRadio=document.getElementById("fahrenheitToCelsius").checked;
+    if(celsiusToFahrenheitRadio)
+        celsiusToFahrenheit();
+    else if(fahrenheitToCelsiusRadio)
+        fahrenheitToCelsius();
+    else
+        document.getElementById("result").textContent="Please select a conversion type.";
+}
